e7451a1be7bf0a04265bc63916d340331aef6c5a
name <qkdkran123@gmail.com>
2020-11-05 17:13:00 +0900

    네이버 로그인 버튼

--- AIPublisher/mysite/AI_Publisher/urls.py
+++ AIPublisher/mysite/AI_Publisher/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.index),
     path('main/', views.main_page),
     path('main/select_character/', views.select_character),
-    path('main/select_character/setting_character/', views.setting_character),
+    path('main/setting_character/', views.setting_character),
+    path('main/setting_relation/', views.setting_relation),
+    path('main/story_make/', views.story_make),
+    path('main/cover_make/', views.cover_make),
     path('main/gallery/', views.gallery),
 ]
--- AIPublisher/mysite/AI_Publisher/views.py
+++ AIPublisher/mysite/AI_Publisher/views.py
@@ -14,6 +14,15 @@ def select_character(request):
 def setting_character(request):
     return render(request, 'AI_Publisher/setting_character.html')
 
+def setting_relation(request):
+    return render(request, 'AI_Publisher/setting_relation.html')
+
+def story_make(request):
+    return render(request, 'AI_Publisher/story_make.html')
+
+def cover_make(request):
+    return render(request, 'AI_Publisher/cover_make.html')
+
 def gallery(request):
     return render(request, 'AI_Publisher/gallery_list.html')
 # Create your views here.
