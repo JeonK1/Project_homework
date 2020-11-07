4be979a470ed78adc002ab78c726fa472a1f3e3d
name <qkdkran123@gmail.com>
2020-11-07 16:39:48 +0900

    style sheet

--- AIPublisher/mysite/AI_Publisher/urls.py
+++ AIPublisher/mysite/AI_Publisher/urls.py
@@ -4,11 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('main/', views.main_page),
-    path('main/select_character/', views.select_character),
-    path('main/setting_character/', views.setting_character),
-    path('main/setting_relation/', views.setting_relation),
-    path('main/story_make/', views.story_make),
-    path('main/cover_make/', views.cover_make),
-    path('main/gallery/', views.gallery),
+    # path('main/', views.main_page),
 ]
--- AIPublisher/mysite/AI_Publisher/views.py
+++ AIPublisher/mysite/AI_Publisher/views.py
@@ -5,24 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'AI_Publisher/login_page.html')
 
-def main_page(request):
-    return render(request, 'AI_Publisher/main_page.html')
 
-def select_character(request):
-    return render(request, 'AI_Publisher/select_character.html')
 
-def setting_character(request):
-    return render(request, 'AI_Publisher/setting_character.html')
 
-def setting_relation(request):
-    return render(request, 'AI_Publisher/setting_relation.html')
 
-def story_make(request):
-    return render(request, 'AI_Publisher/story_make.html')
-
-def cover_make(request):
-    return render(request, 'AI_Publisher/cover_make.html')
-
-def gallery(request):
-    return render(request, 'AI_Publisher/gallery_list.html')
 # Create your views here.
