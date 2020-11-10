0cc7d5a2cf1fcd9512d0fd6ee06e139f967bd822
sp8190 <56158143+sp8190@users.noreply.github.com>
2020-11-10 22:01:21 +0900

    Figma - css 연동... 표지제작,이야기제작 작성

--- AIPublisher/mysite/AI_Publisher/urls.py
+++ AIPublisher/mysite/AI_Publisher/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('main/', views.main_page),
+    path('make_cover/', views.make_cover),
+    path('make_story/', views.make_story),
 ]
--- AIPublisher/mysite/AI_Publisher/views.py
+++ AIPublisher/mysite/AI_Publisher/views.py
@@ -5,11 +5,15 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'AI_Publisher/login_page.html')
 
-
 def main_page(request):
-
     return render(request, 'AI_Publisher/main_page.html')
-    return render(request, 'AI_Publisher/')
+
+def make_cover(request):
+    return render(request, 'AI_Publisher/make_cover.html')
+
+def make_story(request):
+    return render(request, 'AI_Publisher/make_story.html')
+
 
 
 
