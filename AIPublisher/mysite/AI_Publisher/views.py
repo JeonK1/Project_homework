8a201631beef671972c7c9c8b9549a7b1984c365
sp8190 <56158143+sp8190@users.noreply.github.com>
2020-11-23 20:08:27 +0900

    수정

--- AIPublisher/mysite/AI_Publisher/views.py
+++ AIPublisher/mysite/AI_Publisher/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import CharList
+from .models import BookTextList
+from .models import BookContetnsList
+
 
 def index(request):
     return render(request, 'AI_Publisher/login_page.html')
@@ -12,8 +15,33 @@ def main_page(request):
 def make_cover(request):
     return render(request, 'AI_Publisher/make_cover.html')
 
-def make_story(request):
-    return render(request, 'AI_Publisher/make_story.html')
+# def make_story(request):
+#     return render(request, 'AI_Publisher/make_story.html')
+
+class Makestory(ListView):
+    template_name = "AI_Publisher/make_story.html"
+    model = BookTextList
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #
+        # #dummy data
+        # charList = ["1.jpg", "2.jpeg", "3.jpg", "4.jpg"]
+        # charMain = "4.jpg"
+        # card_pos = ['사이좋아요', '아주 좋아요', '약간 좋아요']
+        # card_neu = ['모르는 사이', '애매한듯']
+        # card_neg = ['사이안좋음', '아주 안좋아요', '약간 안좋아요']
+        #
+        # context['charList'] = charList
+        # context['charMain'] = charMain
+        # context['card_pos'] = card_pos
+        # context['card_neu'] = card_neu
+        # context['card_neg'] = card_neg
+        return context
+
+
+
 
 # def set_relation(request):
 #     return render(request, 'AI_Publisher/set_relation.html')
--- AIPublisher/mysite/static/make_story.js
+++ AIPublisher/mysite/static/make_story.js
@@ -1,5 +1,8 @@
-function make_story_move(){
-    // 아래 화살표를 눌렀을 때
+// 사용자 위치
+var count = 0;
+
+// 아래 화살표를 눌렀을 때
+function nextstep_move() {
     // 0-2 발단, 3-5 전개, 6-8 위기, 9-11 절정, 12-14 결말
     $("#question_string").hide();
     $("#question_string_small").hide();
@@ -7,15 +10,10 @@ function make_story_move(){
     $("#RightSidebar").hide();
     $("#camera").hide();
 
-    var arrtext = new Array(5); //배열선언
-    for (var i = 0; i < arrtext.length; i++){
-        arrtext[i] = new Array(3)
-    }
 
-    var count = 0;
-    $("#next_step").click(function() {
+    $("#next_step").click(function () {
         count += 1;
-        switch(count){
+        switch (count) {
             case 1:
                 $("#question_base").hide();
                 $("#question_string").show();
@@ -28,10 +26,10 @@ function make_story_move(){
                 $("#camera").show();
                 break;
             case 3:
-                $("#STEP_1").css("background-color","#D6ECD7");
-                $("#STEP_1").css("box-shadow",'0px 0px 0px');
-                $("#STEP_2").css("background-color","#FFFFFF");
-                $("#STEP_2").css("box-shadow",'0 4px 4px #888');
+                $("#STEP_1").css("background-color", "#D6ECD7");
+                $("#STEP_1").css("box-shadow", '0px 0px 0px');
+                $("#STEP_2").css("background-color", "#FFFFFF");
+                $("#STEP_2").css("box-shadow", '0 4px 4px #888');
 
                 $("#RightSidebar").hide();
                 $("#camera").hide();
@@ -51,10 +49,10 @@ function make_story_move(){
                 $("#camera").show();
                 break;
             case 6:
-                $("#STEP_2").css("background-color","#D6ECD7");
-                $("#STEP_2").css("box-shadow",'0px 0px 0px');
-                $("#STEP_3").css("background-color","#FFFFFF");
-                $("#STEP_3").css("box-shadow",'0 4px 4px #888');
+                $("#STEP_2").css("background-color", "#D6ECD7");
+                $("#STEP_2").css("box-shadow", '0px 0px 0px');
+                $("#STEP_3").css("background-color", "#FFFFFF");
+                $("#STEP_3").css("box-shadow", '0 4px 4px #888');
 
                 $("#RightSidebar").hide();
                 $("#camera").hide();
@@ -74,10 +72,10 @@ function make_story_move(){
                 $("#camera").show();
                 break;
             case 9:
-                $("#STEP_3").css("background-color","#D6ECD7");
-                $("#STEP_3").css("box-shadow",'0px 0px 0px');
-                $("#STEP_4").css("background-color","#FFFFFF");
-                $("#STEP_4").css("box-shadow",'0 4px 4px #888');
+                $("#STEP_3").css("background-color", "#D6ECD7");
+                $("#STEP_3").css("box-shadow", '0px 0px 0px');
+                $("#STEP_4").css("background-color", "#FFFFFF");
+                $("#STEP_4").css("box-shadow", '0 4px 4px #888');
 
                 $("#RightSidebar").hide();
                 $("#camera").hide();
@@ -97,10 +95,10 @@ function make_story_move(){
                 $("#camera").show();
                 break;
             case 12:
-                $("#STEP_4").css("background-color","#D6ECD7");
-                $("#STEP_4").css("box-shadow",'0px 0px 0px');
-                $("#STEP_5").css("background-color","#FFFFFF");
-                $("#STEP_5").css("box-shadow",'0 4px 4px #888');
+                $("#STEP_4").css("background-color", "#D6ECD7");
+                $("#STEP_4").css("box-shadow", '0px 0px 0px');
+                $("#STEP_5").css("background-color", "#FFFFFF");
+                $("#STEP_5").css("box-shadow", '0 4px 4px #888');
 
                 $("#RightSidebar").hide();
                 $("#camera").hide();
@@ -125,7 +123,9 @@ function make_story_move(){
                 break;
         }
     });
+}
 
+function stepbar_move(){
     // STEP 발단 눌렀을 때
     $("#STEP_1").click(function() {
         count = 0;
@@ -235,32 +235,5 @@ function make_story_move(){
         $("#question_base_small").hide();
         $("#question_base").show();
     });
-
-    // text 적기
-    $("#input_text_box").keydown(function(key) {
-        if(key.keyCode == 13){
-
-        }
-    });
-
-    // question_text
-    // 발단
-    // - <주인공> 에게 <배경>은 어떤 곳인가요?
-    // - <주인공>과 <사건>은 어떤 관련이 있나요?
-    // - <주인공> 은 <상대> 에게 왜 <감정1>을 느끼나요?
-    //
-    // 전개
-    // - <주인공>은 어떻게 <사건>을 해결하고자 하나요?
-    // - 왜 <주인공>은 <감정1> 에서 <감정2>가 되었나요?
-    //
-    // 위기
-    // - 왜 <주인공>은 <감정2> 에서 <감정3>가 되었나요?
-    //
-    // 절정
-    // - 왜 <주인공>은 <감정3> 에서 <감정4>가 되었나요?
-    //
-    // 결말
-    // - 왜 <주인공>은 <감정4> 에서 <감정5>가 되었나요?
-    // - 이 이야기의 결말은 어떻게 되나요?
-
 }
+
