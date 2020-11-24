b9b7713ba196dc82376b9db10271089a15999e90
sp8190 <56158143+sp8190@users.noreply.github.com>
2020-11-24 23:22:50 +0900

    배경 고르기 수정

--- AIPublisher/mysite/static/make_cover.js
+++ AIPublisher/mysite/static/make_cover.js
@@ -1,36 +1,33 @@
 function cover_text(){
-
     $("#input_text").keydown(function (key) {
         if (key.keyCode == 13) {
-            alert("sadf")
             $("#Cover_text").text($("#input_text").val());
             $('#input_text').val('');
         }
     });
 }
 
-function cover_text(){
+function select_background(){
 
     $("#Ground_small_1").click(function () {
 
-        $( "#BackGround" ).css( "background-image",($( "#Ground_small_1" ).css( "background-image" )))
-
-
+        $( "#Cover" ).css( "background-image",($( "#Ground_small_1" ).css( "background-image" )));
 
     });
 
     $("#Ground_small_2").click(function () {
 
+        $( "#Cover" ).css( "background-image",($( "#Ground_small_2" ).css( "background-image" )));
 
     });
 
     $("#Ground_small_3").click(function () {
 
-
+        $( "#Cover" ).css( "background-image",($( "#Ground_small_3" ).css( "background-image" )));
     });
 
     $("#Ground_small_4").click(function () {
 
-
+        $( "#Cover" ).css( "background-image",($( "#Ground_small_4" ).css( "background-image" )));
     });
 }
